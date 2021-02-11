53714772338a3fc1130a89a9ec46a885acb39e5f
Takuya <r28.6sa@gmail.com>
2021-02-11 14:35:41 +0900

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,46 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import SignUp from "../views/SignUp.vue";
+import Profile from "../views/Profile.vue";
+import Detail from "../views/Detail.vue";
 
-Vue.use(Vuex)
+Vue.use(VueRouter);
 
-export default new Vuex.Store({
-  state: {
+const routes = [
+  {
+    path: "/",
+    name: "login",
+    component: Login,
   },
-  mutations: {
+  {
+    path: "/signup",
+    name: "signup",
+    component: SignUp,
   },
-  actions: {
+  {
+    path: "/home",
+    name: "Home",
+    component: Home,
   },
-  modules: {
-  }
-})
+  {
+    path: "/detail/:id",
+    name: "detail",
+    component: Detail,
+    props: true,
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: Profile,
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
